326c24c4abf9696e8b35884642916a9198f617fd
zitast <carmen.stippler@web.de>
2018-11-03 19:37:05 +0100

    3 weapons
    Added 3 weapons with shooting or exploding animation.

---
+++ Assets/Scripts/gun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) //left mouse key
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        RaycastHit hit; //what we hit with the ray
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            //hit.transform is the object you shooted on
+            Debug.Log(hit.transform.name);
+
+            enemy zombie = hit.transform.GetComponent<enemy>();
+
+            if (zombie != null)
+            {
+                zombie.TakeDamage(damage);
+            }
+        }
+    }
+}

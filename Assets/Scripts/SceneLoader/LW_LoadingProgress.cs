6d2163b221dcb10772e60c70196c4601837e7493
theskn <naghavi.kianoosh@gmail.com>
2018-11-04 23:03:16 +0100

    SceneLoaderClass
    skn: implemented the sceneloaderclass plus asnyc sceneloading, loadingbar.
    presented along side a functional menu

---
+++ Assets/Scripts/SceneLoader/LW_LoadingProgress.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using LittleWarrior.SceneLoader;
+
+namespace LittleWarrior.SceneLoader
+{
+    public class LW_LoadingProgress : MonoBehaviour
+    {
+
+        private Text LoadingText;
+
+        void Start()
+        {
+            LoadingText = GetComponent<Text>();
+
+            if (LoadingText == null)
+            {
+                Debug.Log(">>>> The LW_LoadingProgress is added to a wrong game object.");
+                Destroy(gameObject);
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            LoadingText.text = LW_SceneLoaderAsync.Instance.LoadingProgress.ToString("F0") + "%";
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader/LW_SceneLoaderAsync.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using LittleWarrior;
+using LittleWarrior.SceneLoader;
+
+namespace LittleWarrior.SceneLoader
+{
+    public class LW_SceneLoaderAsync : LW_Singleton<LW_SceneLoaderAsync>
+    {
+
+        // NOTE (skn): Loading Progress: private setter, public getter
+        private float _loadingProgress;
+        public float LoadingProgress
+        {
+            get
+            {
+                return _loadingProgress;
+            }
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            StartCoroutine(LoadScenesInOrder(sceneName));
+        }
+
+        private IEnumerator LoadScenesInOrder(string sceneName)
+        {
+            // NOTE (skn): LoadSceneAsync() returns an AsyncOperation, 
+            //             so will only continue past this point when the 
+            //             Operation has finished
+            yield return SceneManager.LoadSceneAsync("Loading");
+
+            // NOTE (skn): As soon as we've finished loading the loading screen, 
+            //             start loading the game scene
+            yield return StartCoroutine(LoadFinalScene(sceneName));
+        }
+
+        private IEnumerator LoadFinalScene(string sceneName)
+        {
+            var asyncScene = SceneManager.LoadSceneAsync(sceneName);
+
+            // NOTE (skn): This value stops the scene from displaying when it's finished 
+            //             loading
+            asyncScene.allowSceneActivation = false;
+
+            while (!asyncScene.isDone)
+            {
+                // NOTE (skn): Loading bar progress
+                _loadingProgress = Mathf.Clamp01(asyncScene.progress / 0.9f) * 100;
+
+                // NOTE (skn): Scene has loaded as much as possible, the last 10% can't 
+                //             be multi-threaded
+                if (asyncScene.progress >= 0.9f)
+                {
+                    // NOTE (skn): Brings up the scene
+                    asyncScene.allowSceneActivation = true;
+                }
+
+                yield return null;
+            }
+        }
+    }
+
+}
